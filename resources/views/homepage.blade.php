60d034db5a7fd8abe28028f5ec0f2c00ef602729
D43m13N <d43m13n@outlook.com>
2023-07-24 01:20:26 +0200

    new commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,7 +12,7 @@ $today = date("j F Y");
     Mijn naam is Rafael en ik ben een junior webdeveloper. ik ben bekend met de talen ruby, on rails, php, laravel, html, css, een beetje javascript en ik ben nog van 
     plan om meer te leren.
 
-    voor meer informatie neem verwijs ik je door naar mijn cv of naar de projecten.
+    voor meer informatie neem verwijs ik je door naar mijn cv of naar de projecten....
 
     
 
